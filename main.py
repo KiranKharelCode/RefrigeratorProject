c47c5c02f8113c7665b97d143cee7b53b13efcf6
KiranKharelCode <142636458+KiranKharelCode@users.noreply.github.com>
2023-08-19 21:29:03 -0600

    updated main.py
    Put the part of the code used for sending the text message inside a function called "send_text"

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from twilio.rest import Client
 import os
 
-account_sid = os.environ["SID"]
-auth_token = os.environ["TOKEN"]
-client = Client(account_sid, auth_token)
+def send_text():
+    account_sid = os.environ["SID"]
+    auth_token = os.environ["TOKEN"]
+    client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-  from_=os.environ['FROM_NUMBER'],
-  body='This is a test',
-  to=os.environ["TO_NUMBER"]
-)
+    message = client.messages.create(
+      from_=os.environ['FROM_NUMBER'],
+      body='This is a test',
+      to=os.environ["TO_NUMBER"]
+    )
 
-print(message.sid)
+    print(message.sid)
