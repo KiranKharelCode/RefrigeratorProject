5997535de188ec3f61849f658e511c65bbd77404
KiranKharelCode <142636458+KiranKharelCode@users.noreply.github.com>
2023-08-21 22:01:06 -0600

    HTML page
    Html page that is going to serve as a UI for the data, so I can manipulate the data from my phone.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask,request,redirect,Response
+from flask import Flask,request,redirect,Response,render_template
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import os
 
-def send_sms():
+def send_sms(sent_sms):
     account_sid = os.environ["SID"]
     auth_token = os.environ["TOKEN"]
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
     from_=os.environ['FROM_NUMBER'],
-    body='This is a test',
+    body=f"{sent_sms}",
     to=os.environ["TO_NUMBER"])
 
 
@@ -18,10 +18,13 @@ app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    resp = MessagingResponse()
     body = request.values.get('Body', None)
-    print(body)
-    return str(resp)
+    if "hello" in str(body):
+        send_sms("Hello detected")
+    else:
+        pass
+
+    return render_template('Data.html')
 
 
 if __name__ == "__main__":
