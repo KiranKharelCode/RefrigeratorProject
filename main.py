de1e25b2b04f11946e9c628b88c2a43f030ef134
KiranKharelCode <142636458+KiranKharelCode@users.noreply.github.com>
2023-08-22 21:54:06 -0600

    design of data input
    a draft design of how the user will input data

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask,request,redirect,Response,render_template
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+from wtforms import Form, StringField, validators
 import os
 
+
+class FoodDataForm(Form):
+    foods = StringField('Foods')
 def send_sms(sent_sms):
     account_sid = os.environ["SID"]
     auth_token = os.environ["TOKEN"]
@@ -15,16 +19,18 @@ def send_sms(sent_sms):
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['KEY']
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
+    form = FoodDataForm()
+    if request.method == "POST":
+        print(request.form.get('foods'))
     body = request.values.get('Body', None)
     if "hello" in str(body):
         send_sms("Hello detected")
-    else:
-        pass
 
-    return render_template('Data.html')
+    return render_template('Data.html',form=form)
 
 
 if __name__ == "__main__":
