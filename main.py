9c592a66750d8bba42a5ee43097ecce3018e5e0d
KiranKharelCode <142636458+KiranKharelCode@users.noreply.github.com>
2023-08-27 21:49:01 -0600

    Organizing Data
    Cleaning the code a little

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 
 class FoodDataForm(Form):
     foods = StringField('Foods')
+
 def send_sms(sent_sms):
     account_sid = os.environ["SID"]
     auth_token = os.environ["TOKEN"]
@@ -16,23 +17,33 @@ def send_sms(sent_sms):
     from_=os.environ['FROM_NUMBER'],
     body=f"{sent_sms}",
     to=os.environ["TO_NUMBER"])
+    return render_template('Message.html',message=f'{sent_sms}')
+
+def order_data(data):
+    print(data)
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['KEY']
 
-@app.route("/sms", methods=['GET', 'POST'])
+@app.route('/sms',methods=['GET','POST'])
 def sms_reply():
+    body = request.values.get('Body', None)
+    if "hello" in str(body):
+        return send_sms("Hello detected")
+
+    return render_template("Message.html",message="Message sent")
+
+@app.route("/add", methods=['GET', 'POST'])
+def add_items():
     list = []
     form = FoodDataForm()
     if request.method == "POST":
         print(request.form.get('foods'))
-        for items in zip(request.form.getlist('INPUT2')):
-            list.append(items)
-        print(list)
-    body = request.values.get('Body', None)
-    if "hello" in str(body):
-        send_sms("Hello detected")
+        for items,value in zip(request.form.getlist('food_input'),request.form.getlist('quantity')):
+            list.append((items,value))
+        order_data(list)
+
 
     return render_template('Data.html',form=form)
 
--- static/main.js
+++ static/main.js
@@ -2,11 +2,15 @@ const button = document.getElementById("ADD_BUTTON");
 button.addEventListener('click',function(){
 
     console.log("CLICKED");
-    const para = document.createElement("input");
-    para.setAttribute("name","INPUT2")
+    const food_input = document.createElement("input");
+    food_input.setAttribute("name","food_input")
+
+    const quantity = document.createElement("input");
+    quantity.setAttribute("name","quantity")
 
     const element = document.getElementById("first_div");
-    element.appendChild(para);
+    element.appendChild(food_input);
+    element.appendChild(quantity);
 
 
 });
