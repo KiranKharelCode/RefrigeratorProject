3d7efc2d48f58c5aa33a582538e5435e987863a2
KiranKharelCode <142636458+KiranKharelCode@users.noreply.github.com>
2023-09-17 19:49:31 -0600

    updated automation file

--- ChatGTP.py
+++ ChatGTP.py
@@ -27,16 +27,13 @@ chrome_options.add_experimental_option("useAutomationExtension", False)
 # Setting the driver path and requesting a page
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://chat.openai.com/auth/login')
-time.sleep(3)
-try:
-    WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div[1]/div[2]/div[1]/div/button[1]'))).click()
-    WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="username"]'))).send_keys(os.environ["CHATGTP_EMAIL"])
-    WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.XPATH,'/html/body/div/main/section/div/div/div/div[1]/div/form/div[2]/button'))).click()
-    WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.XPATH,'// *[ @ id = "password"]'))).send_keys(os.environ['CHATGTP_PASS'])
-    WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/main/section/div/div/div/form/div[3]/button'))).click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '// *[ @ id = "radix-:rf:"] / div[2] / div / div[4] / button'))).click()
+driver.get('https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx')
+time.sleep(5)
+button = driver.execute_script('return document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelector("#cib-chat-main > cib-welcome-container").shadowRoot.querySelector("div.controls > cib-tone-selector").shadowRoot.querySelector("#tone-options > li:nth-child(3) > button")')
+driver.execute_script('arguments[0].click();',button)
+text_box = driver.execute_script('return document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("div > div.main-container > div > div.input-row > cib-text-input").shadowRoot.querySelector("#searchbox").value = "TestNG";')
 
 
-except:
-    pass
+
+
+
